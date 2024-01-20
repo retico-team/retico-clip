6524713e3f3c02e08668340567397256635abc95
Casey Kennington <bakuzen@gmail.com>
2024-01-19 18:37:15 -0700

    working now with SAM and ExtractObjectsModule

--- README.md
+++ README.md
@@ -3,38 +3,56 @@ A ReTiCo module for CLIP. See below for more information.
 
 ### Installation and requirements
 
+See https://github.com/openai/CLIP
+
+`pip install git+https://github.com/openai/CLIP.git`
+
 ### Example
 ```python
-import sys
-from retico import *
+import sys, os
 
-prefix = '/path/to/modules/'
-sys.path.append(prefix+'retico-clip')
+os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
+
+prefix = '/path/to/prefix/'
+sys.path.append(prefix+'retico-core')
 sys.path.append(prefix+'retico-vision')
-sys.path.append(prefix+'retico-yolov8')
+sys.path.append(prefix+'retico-sam')
+sys.path.append(prefix+'retico-dino')
+sys.path.append(prefix+'retico-clip')
 
-# from retico_yolov8 import YoloV8
-from retico_clip.clip import ClipObjectFeatures
+from retico_core import *
+from retico_core.debug import DebugModule
 from retico_vision.vision import WebcamModule 
-from retico_yolov8.yolov8 import Yolov8
+from retico_vision.vision import ExtractObjectsModule
+from retico_sam.sam import SAMModule
+from retico_clip.clip import ClipObjectFeatures
 
-
+path_var = 'sam_vit_h_4b8939.pth'
 
 webcam = WebcamModule()
-yolo = Yolov8()
+sam = SAMModule(model='h', path_to_chkpnt=path_var, use_bbox=True)  
+extractor = ExtractObjectsModule(num_obj_to_display=1)  
 feats = ClipObjectFeatures(show=True)
-debug = modules.DebugModule()
+debug = DebugModule()  
 
-webcam.subscribe(yolo)
-yolo.subscribe(feats)
+webcam.subscribe(sam)  
+sam.subscribe(extractor)  
+extractor.subscribe(feats)    
 feats.subscribe(debug)
 
-run(webcam)
+webcam.run()  
+sam.run()  
+extractor.run()  
+feats.run()
+debug.run()  
 
 print("Network is running")
 input()
 
-stop(webcam)
+webcam.stop()  
+sam.stop()  
+extractor.stop()   
+debug.stop()  
 ```
 
 
--- retico_clip/clip.py
+++ retico_clip/clip.py
@@ -2,13 +2,14 @@
 CLIP Module
 ==================
 
-This module provides extracts features from DetectedObjectsIU using CLIP.
+This module provides extracts features from ExtractedObjectsIU using CLIP.
 """
 
 import clip
 from collections import deque
 import numpy as np
 import threading
+from PIL import Image
 import time
 from retico_core.abstract import IncrementalQueue
 import torch
@@ -17,12 +18,12 @@ import retico_core
 # TODO make is so that you don't need these 3 lines below
 # idealy retico-vision would be in the env so you could 
 # import it by just using:
-# from retico_vision.vision import ImageIU, DetectedObjectsIU
+# from retico_vision.vision import ImageIU, ExtractedObjectsIU
 import sys
-prefix = '../../'
-sys.path.append(prefix+'retico-vision')
+# prefix = '../../'
+# sys.path.append(prefix+'retico-vision')
 
-from retico_vision.vision import DetectedObjectsIU, ObjectFeaturesIU
+from retico_vision.vision import ExtractedObjectsIU, ObjectFeaturesIU
 
 class ClipObjectFeatures(retico_core.AbstractModule):
     @staticmethod
@@ -35,18 +36,19 @@ class ClipObjectFeatures(retico_core.AbstractModule):
 
     @staticmethod
     def input_ius():
-        return [DetectedObjectsIU]
+        return [ExtractedObjectsIU]
 
     @staticmethod
     def output_iu():
         return ObjectFeaturesIU
     
-    def __init__(self, model_name = "ViT-B/32", show=False, **kwargs):
+    def __init__(self, model_name = "ViT-B/32", show=False, top_objects=1, **kwargs):
         super().__init__(**kwargs)
         self.model_name = model_name
         self.model = None
         self.preprocess = None
         self.show = show
+        self.top_objects = top_objects
         self.queue = deque(maxlen=1)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -57,24 +59,24 @@ class ClipObjectFeatures(retico_core.AbstractModule):
             else:
                 self.queue.append(iu)
 
-    def get_clip_subimage(self, I, img_box):
-        # expected format:
-        # Numpy array, length 4, [xmin, ymin, xmax, ymax]
+    # def get_clip_subimage(self, I, img_box):
+    #     # expected format:
+    #     # Numpy array, length 4, [xmin, ymin, xmax, ymax]
 
-        xmin = int(img_box[0])
-        xmax = int(img_box[2])
-        ymin = int(img_box[1])
-        ymax = int(img_box[3])
-        sub = I.crop([xmin,ymin,xmax,ymax])
+    #     xmin = int(img_box[0])
+    #     xmax = int(img_box[2])
+    #     ymin = int(img_box[1])
+    #     ymax = int(img_box[3])
+    #     sub = I.crop([xmin,ymin,xmax,ymax])
 
-        if self.show:
-            import cv2
-            img_to_show = np.asarray(sub)
-            cv2.imshow('image',cv2.cvtColor(img_to_show, cv2.COLOR_RGB2BGR)) 
-            cv2.waitKey(1)
-        # pim = PImage.fromarray(sub)
-        sub.load()
-        return sub 
+    #     if self.show:
+    #         import cv2
+    #         img_to_show = np.asarray(sub)
+    #         cv2.imshow('image',cv2.cvtColor(img_to_show, cv2.COLOR_RGB2BGR)) 
+    #         cv2.waitKey(1)
+    #     # pim = PImage.fromarray(sub)
+    #     sub.load()
+    #     return sub 
     
     def _extractor_thread(self):
         while self._extractor_thread_active:
@@ -84,11 +86,22 @@ class ClipObjectFeatures(retico_core.AbstractModule):
 
             input_iu = self.queue.popleft()
             image = input_iu.image
-            detected_objects = input_iu.detected_objects
+            detected_objects = input_iu.extracted_objects
             object_features = {}
 
             for i, obj in enumerate(detected_objects):
-                sub_img = self.get_clip_subimage(image, obj)
+                # sub_img = self.get_clip_subimage(image, obj)
+                if i>=self.top_objects: break
+                sub_img = detected_objects[obj]
+                
+                if self.show:
+                    import cv2
+                    img_to_show = np.asarray(sub_img)
+                    cv2.imshow('image',cv2.cvtColor(img_to_show, cv2.COLOR_RGB2BGR)) 
+                    cv2.waitKey(1)
+
+                sub_img = Image.fromarray(sub_img)
+                sub_img.load()
 
                 with torch.no_grad():
                     img = self.preprocess(sub_img).unsqueeze(0).to(self.device)
