3682dc918d57eb801c8a1fd7d6343d4c340e5dde
Dalton Bilau Goncalves <daltonbilaugonca@u.boisestate.edu>
2026-08-12 16:13:34 -0600

    Updated pyp

--- README.md
+++ README.md
@@ -1,59 +1,85 @@
-# retico-clip
-A ReTiCo module for CLIP. See below for more information.
+# retico_clip
+A ReTico module for CLIP visual feature embeddings from objects detected within an image.
 
-### Installation and requirements
+## Installation and Requirements
 
-See https://github.com/openai/CLIP
+To use the CLIP module you first need to install the retico-core and retico-vision packages:
+Install retico_core:
+* ````pip install git+https://github.com/retico-team/retico-core.git```
+Install retico_vision:
+* ```pip install git+https://github.com/retico-team/retico-vision.git```
 
-`pip install setuptools==70.0`
-`pip install git+https://github.com/openai/CLIP.git`
+Right after that, install OpenAI's CLIP package:
+* Install clip:
+`````pip install git+https://github.com/openai/CLIP.git```
+
+Then install the retico-clip package:
+* Install retico-clip:
+````pip install git+https://github.com/retico-team/retico-clip.git```
+
+## Modules
+
+### `ClipModule`
+Subscribes to `ExtractedObjectsIU` (produced by `ExtractObjectsModule`) and encodes each cropped object image using CLIP, producing an `ObjectFeaturesIU` containing a dictionary of feature vectors, one per successfully encoded object.
+
+#### Arguments:
+* `model_name` (str): the CLIP model checkpoint to load; defaults to `'ViT-B/32'`
+* `show` (bool): if `True`, opens a live `cv2` preview window showing each object crop as it's processed; useful for visually confirming detections and crop quality; defaults to `False`
+* `top_objects` (int): the maximum number of valid (non-empty) objects to encode per incoming frame; objects with degenerate/empty crops are skipped and do not count against this limit; defaults to `1`
+
+#### Arguments
+* `model_name` (str): the CLIP model checkpoint to load; defaults to `'ViT-B/32'`
+* `show` (bool): if `True`, opens a live `cv2` preview window showing each object crop as it's processed; useful for visually confirming detections and crop quality; defaults to `False`
+* `top_objects` (int): the maximum number of valid (non-empty) objects to encode per incoming frame; objects with degenerate/empty crops are skipped and do not count against this limit; defaults to `1`
 
 ### Example
+**Note:** The example runner uses `ExtractObjectsModule` from `retico_vision`. The Extract Objects module allows the user to determine how many images to plot to be displayed
+
 ```python
 import sys, os
 
-os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 
-prefix = '/path/to/prefix/'
-sys.path.append(prefix+'retico-core')
-sys.path.append(prefix+'retico-vision')
-sys.path.append(prefix+'retico-sam')
-sys.path.append(prefix+'retico-dino')
-sys.path.append(prefix+'retico-clip')
+prefix = "/path/to/prefix/"
+sys.path.append(prefix + "retico-core")
+sys.path.append(prefix + "retico-vision")
+sys.path.append(prefix + "retico-sam")
+sys.path.append(prefix + "retico-dino")
+sys.path.append(prefix + "retico-clip")
 
 from retico_core import *
 from retico_core.debug import DebugModule
-from retico_vision.vision import WebcamModule 
+from retico_vision.vision import WebcamModule
 from retico_vision.vision import ExtractObjectsModule
 from retico_sam.sam import SAMModule
 from retico_clip.clip import ClipObjectFeatures
 
-path_var = 'sam_vit_h_4b8939.pth'
+path_var = "sam_vit_h_4b8939.pth"
 
 webcam = WebcamModule()
-sam = SAMModule(model='h', path_to_chkpnt=path_var, use_bbox=True)  
-extractor = ExtractObjectsModule(num_obj_to_display=1)  
+sam = SAMModule(model="h", path_to_chkpnt=path_var, use_bbox=True)
+extractor = ExtractObjectsModule(num_obj_to_display=1)
 feats = ClipObjectFeatures(show=True)
-debug = DebugModule()  
+debug = DebugModule()
 
-webcam.subscribe(sam)  
-sam.subscribe(extractor)  
-extractor.subscribe(feats)    
+webcam.subscribe(sam)
+sam.subscribe(extractor)
+extractor.subscribe(feats)
 feats.subscribe(debug)
 
-webcam.run()  
-sam.run()  
-extractor.run()  
+webcam.run()
+sam.run()
+extractor.run()
 feats.run()
-debug.run()  
+debug.run()
 
 print("Network is running")
 input()
 
-webcam.stop()  
-sam.stop()  
-extractor.stop()   
-debug.stop()  
+webcam.stop()
+sam.stop()
+extractor.stop()
+debug.stop()
 ```
 
 
--- retico_clip/clip.py
+++ retico_clip/clip.py
@@ -15,15 +15,18 @@ from retico_core.abstract import IncrementalQueue
 import torch
 
 import retico_core
+
 # TODO make is so that you don't need these 3 lines below
-# idealy retico-vision would be in the env so you could 
+# idealy retico-vision would be in the env so you could
 # import it by just using:
 # from retico_vision.vision import ImageIU, ExtractedObjectsIU
 import sys
+
 # prefix = '../../'
 # sys.path.append(prefix+'retico-vision')
 
 from retico_vision.vision import ExtractedObjectsIU, ObjectFeaturesIU
+
 
 class ClipObjectFeatures(retico_core.AbstractModule):
     @staticmethod
@@ -41,8 +44,8 @@ class ClipObjectFeatures(retico_core.AbstractModule):
     @staticmethod
     def output_iu():
         return ObjectFeaturesIU
-    
-    def __init__(self, model_name = "ViT-B/32", show=False, top_objects=1, **kwargs):
+
+    def __init__(self, model_name="ViT-B/32", show=False, top_objects=1, **kwargs):
         super().__init__(**kwargs)
         self.model_name = model_name
         self.model = None
@@ -72,12 +75,12 @@ class ClipObjectFeatures(retico_core.AbstractModule):
     #     if self.show:
     #         import cv2
     #         img_to_show = np.asarray(sub)
-    #         cv2.imshow('image',cv2.cvtColor(img_to_show, cv2.COLOR_RGB2BGR)) 
+    #         cv2.imshow('image',cv2.cvtColor(img_to_show, cv2.COLOR_RGB2BGR))
     #         cv2.waitKey(1)
     #     # pim = PImage.fromarray(sub)
     #     sub.load()
-    #     return sub 
-    
+    #     return sub
+
     def _extractor_thread(self):
         while self._extractor_thread_active:
             if len(self.queue) == 0:
@@ -85,44 +88,60 @@ class ClipObjectFeatures(retico_core.AbstractModule):
                 continue
 
             input_iu = self.queue.popleft()
-            image = input_iu.image
-            detected_objects = input_iu.extracted_objects
-            object_features = {}
+            try:
+                image = input_iu.image
+                detected_objects = input_iu.extracted_objects
+                object_features = {}
 
-            for i, obj in enumerate(detected_objects):
-                # sub_img = self.get_clip_subimage(image, obj)
-                if i>=self.top_objects: break
-                sub_img = detected_objects[obj]
-                
-                # if self.show:
-                #     import cv2
-                #     img_to_show = np.asarray(sub_img)
-                #     cv2.imshow('image',cv2.cvtColor(img_to_show, cv2.COLOR_RGB2BGR)) 
-                #     cv2.waitKey(1)
+                for i, obj in enumerate(detected_objects):
+                    # sub_img = self.get_clip_subimage(image, obj)
+                    if i >= self.top_objects:
+                        break
+                    sub_img = detected_objects[obj]
 
-                if self.show:
+                    if sub_img is None:
+                        continue
+                    w, h = sub_img.size
+                    if w == 0 or h == 0:
+                        continue
+
+                    if self.show:
+                        import cv2
+
+                        img_to_show = np.asarray(sub_img)
+                        cv2.imshow(
+                            "image", cv2.cvtColor(img_to_show, cv2.COLOR_RGB2BGR)
+                        )
+                        cv2.waitKey(1)
+
+                    # if self.show:
                     # print(sub.getbands())
                     # sub = sub.convert("BGR")
-                    sub.show()                
+                    #    sub.show()
 
-                # sub_img = Image.fromarray(sub_img)
-                # sub_img.load()
+                    # sub_img = Image.fromarray(sub_img)
+                    # sub_img.load()
 
-                with torch.no_grad():
-                    img = self.preprocess(sub_img).unsqueeze(0).to(self.device)
-                    yhat = self.model.encode_image(img).cpu().numpy()
-                    object_features[i] = yhat.tolist()
+                    with torch.no_grad():
+                        img = self.preprocess(sub_img).unsqueeze(0).to(self.device)
+                        yhat = self.model.encode_image(img).cpu().numpy()
+                        object_features[i] = yhat.tolist()
 
-            output_iu = self.create_iu(input_iu)
-            output_iu.set_object_features(image, object_features)
-            um = retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
-            self.append(um)
-    
+                output_iu = self.create_iu(input_iu)
+                output_iu.set_object_features(image, object_features)
+                um = retico_core.UpdateMessage.from_iu(
+                    output_iu, retico_core.UpdateType.ADD
+                )
+                self.append(um)
+
+            except Exception as e:
+                print(f"Error processing input IU: {e}")
+                continue
 
     def prepare_run(self):
         self.model, self.preprocess = clip.load(self.model_name, self.device)
         self._extractor_thread_active = True
         threading.Thread(target=self._extractor_thread).start()
-    
+
     def shutdown(self):
         self._extractor_thread_active = False
